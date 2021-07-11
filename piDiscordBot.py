3bbc0e4bf08a7ce9dd6639dcc45780ffe42d6599
Trevor Hillsgrove <hillsgrove.trevor@gmail.com>
2021-07-10 20:38:42 -0400

    Initial changes to support translating different Gmail labels into Discord messages (with support for different override channels)

--- piDiscordBot.py
+++ piDiscordBot.py
@@ -121,17 +121,18 @@ def downloadPhotosFromEmail(gmailService, emailData):
                 f.write(file_data)
             counter = counter + 1
 
-async def uploadPhotosFromDownloaded(discordClient, loadedConfig):
+async def uploadPhotosFromDownloaded(discordClient, loadedConfig, channelToSendTo):
     """Uploads downloaded files from downloadPhotosFromEmail to Discord
 
     Args:
       discordClient: Discord client
       loadedConfig: configuration from yaml file
+      channelToSendTo: channel id to send message to
 
     Returns:
       Void, uploads images to Discord, and then deletes the file it uploaded from downloadedGmailPhotos
     """
-    discordChannel = discordClient.get_channel(loadedConfig['discord']['msgChannelId'])
+    discordChannel = discordClient.get_channel(channelToSendTo)
     # Files to upload
     filePath = './downloadedGmailPhotos'
     imagesToUpload = [f for f in listdir(filePath) if isfile(join(filePath, f))]
@@ -139,13 +140,14 @@ async def uploadPhotosFromDownloaded(discordClient, loadedConfig):
         await discordChannel.send(file=discord.File(filePath + '/' + fileVal))
         remove(filePath + '/' + fileVal)
 
-async def sendTextFromEmail(emailData, discordClient, loadedConfig):
+async def sendTextFromEmail(emailData, discordClient, loadedConfig, channelToSendTo):
     """Uploads the text contained in the email (versus the attachments)
 
     Args:
       emailData: Email data to get text from
       discordClient: Discord client
       loadedConfig: configuration from yaml file
+      channelToSendTo: channel id to send message to
 
     Returns:
       Void, uploads text from email to Discord
@@ -167,15 +169,48 @@ async def sendTextFromEmail(emailData, discordClient, loadedConfig):
                 e = sys.exc_info()[1]
                 print(e)
 
-            discordChannel = discordClient.get_channel(loadedConfig['discord']['msgChannelId'])
+            discordChannel = discordClient.get_channel(channelToSendTo)
             await discordChannel.send(messageText)
 
-def getGmailVideoLabel(gmailService, loadedConfig):
+async def sendSubjectLineFromEmail(emailData, discordClient, loadedConfig, channelToSendTo):
+    """Uploads the subject line text to a Discord message
+
+    Args:
+      emailData: Email data to get subject line text from
+      discordClient: Discord client
+      loadedConfig: configuration from yaml file
+      channelToSendTo: channel id to send message to
+
+    Returns:
+      Void, uploads text from subject line of an email to Discord
+    """
+
+    for part in emailData['payload']['parts']:
+        if part['mimeType'] == 'text/plain':
+            base64text = part['body']['data']
+            # https://gist.github.com/perrygeo/ee7c65bb1541ff6ac770
+            # apparently there's no padding in certain cases, so we need to add it in
+            # we can add arbitrary padding length in, it'll ignore the rest of it.
+            toDecode = base64text + '======================='
+
+            messageText = ''
+            try:
+                # Parse body text
+                messageText = base64.b64decode(toDecode).decode('utf-8')
+            except:
+                e = sys.exc_info()[1]
+                print(e)
+
+            discordChannel = discordClient.get_channel(channelToSendTo)
+            await discordChannel.send(messageText)
+
+def getGmailLabel(gmailService, loadedConfig, labelKeyName):
     """Gets a Gmail label id, given the gmail service and string value defined in the config
 
     Args:
       gmailService: Gmail service
       loadedConfig: configuration from yaml file
+      labelKeyName: Label key in loadedConfig that contains the proper human-friendly label name
     Returns:
       Numerical string for the Gmail label id
     """
@@ -186,20 +221,38 @@ def getGmailVideoLabel(gmailService, loadedConfig):
     if labels:
         labelId = ''
         for label in labels:
-            if label['name'] == loadedConfig['gmail']['videoLabelName']:
+            if label['name'] == loadedConfig['gmail'][labelKeyName]:
                 labelId = label['id']
                 break
     return labelId
 
-async def sendGmailAsDiscord(labelId, discordClient, gmailService, loadedConfig):
+async def getLabelSendingChannel(labelName, loadedConfig):
+    """Gets the channel for sending a message to, from a given labelName and the loaded config
+
+    Args:
+      labelName: Label name to get the channel id for
+      loadedConfig: configuration from yaml file
+    Returns:
+      String id of a Discord channel, for the given labelName
+    """
+
+    channelToSendTo = loadedConfig['discord']['msgChannelId']
+    if 'msgChannelOverride' in loadedConfig['discord']['gmail'][labelName]:
+        channelToSendTo = loadedConfig['discord']['gmail'][labelName]['msgChannelOverride']
+    
+    return channelToSendTo
+
+
+async def sendGmailAsDiscord(labelId, discordClient, gmailService, loadedConfig, channelToSendTo):
     """Main function to convert all unread Gmail emails under a certain label (defined in configs) to a series of Discord messages
     Args:
       labelId: Gmail label id
       discordClient: Discord client
       gmailService: Gmail service
       loadedConfig: configuration from yaml file
+      channelToSendTo: channel id to send message to
     Returns:
-      Void, Sens email photos and message content as Discord messages
+      Void, Sends email photos and message content as Discord messages
     """
 
     emails = getUnreadEmails(labelId, gmailService)
@@ -209,8 +262,8 @@ async def sendGmailAsDiscord(labelId, discordClient, gmailService, loadedConfig)
             emailData = gmailService.users().messages().get(userId='me', id=email['id']).execute()
 
             downloadPhotosFromEmail(gmailService, emailData)
-            await uploadPhotosFromDownloaded(discordClient, loadedConfig)
-            await sendTextFromEmail(emailData, discordClient, loadedConfig)
+            await uploadPhotosFromDownloaded(discordClient, loadedConfig, channelToSendTo)
+            await sendTextFromEmail(emailData, discordClient, loadedConfig, channelToSendTo)
 
             labelToRemove = {
                 "removeLabelIds": [
@@ -229,9 +282,51 @@ async def sendGmailAsDiscord(labelId, discordClient, gmailService, loadedConfig)
             print('exception sendGmailAsDiscord')
             continue
 
+async def sendGmailSubjectAsDiscord(labelId, discordClient, gmailService, loadedConfig):
+    """Main function to convert all unread Gmail emails under a certain label (defined in configs) to a discord message (specifically, the email subject)
+    Args:
+      labelId: Gmail label id
+      discordClient: Discord client
+      gmailService: Gmail service
+      loadedConfig: configuration from yaml file
+    Returns:
+      Void, Sends email subject line content as a Discord message
+    """
+
+    emails = getUnreadEmails(labelId, gmailService)
+
+    for email in emails:
+
+        print(email)
+
+        try:
+            emailData = gmailService.users().messages().get(userId='me', id=email['id']).execute()
+
+            await sendSubjectLineFromEmail(emailData, discordClient, loadedConfig)
+
+            labelToRemove = {
+                "removeLabelIds": [
+                    'UNREAD'
+                ],
+                "addLabelIds": []
+            }
+            try:
+                # Remove unread label
+                gmailService.users().messages().modify(userId='me', id=emailData['id'], body=labelToRemove).execute()
+            except:
+                pass
+
+        except Exception as e:
+            print(e)
+            print('exception sendGmailSubjectAsDiscord')
+            continue
+
 # Discord client class
 class MyClient(discord.Client):
     videoLabelId = None
+    videoLabelSendingChannel = None
+    choreLabelId = None
+    choreLabelSendingChannel = None
     gmailService = None
     loadedConfig = None
 
@@ -259,7 +354,8 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
         if message.content == '!checkEmail':
-            await sendGmailAsDiscord(videoLabelId, self, gmailService, loadedConfig)
+            await sendGmailAsDiscord(videoLabelId, self, gmailService, loadedConfig, videoLabelSendingChannel)
+            await sendGmailSubjectAsDiscord(choreLabelId, self, gmailService, loadedConfig, choreLabelSendingChannel)
             print('Check Email Manually triggered')
 
 # Generic helper functions
@@ -283,17 +379,27 @@ loadedConfig = yaml.safe_load(open("./piDiscordConfig.yaml"))
 
 # Gmail initialization
 gmailService = gmailInitialize(loadedConfig)
-videoLabelId = getGmailVideoLabel(gmailService, loadedConfig)
+videoLabelId = getGmailLabel(gmailService, loadedConfig, 'videoLabelName')
+videoLabelSendingChannel = getLabelSendingChannel('videoLabelName, loadedConfig')
+choreLabelId = getGmailLabel(gmailService, loadedConfig, 'choreLabelName')
+choreLabelSendingChannel = getLabelSendingChannel('choreLabelName', 'loadedConfig')
 
 # Discord client initialization
 client = MyClient()
 if len(videoLabelId):
     client.videoLabelId = videoLabelId
+    client.videoLabelSendingChannel = videoLabelSendingChannel
+    client.choreLabelId = choreLabelId
+    client.choreLabelSendingChannel = choreLabelSendingChannel
     client.gmailService = gmailService
     client.loadedConfig = loadedConfig
 
-    # Start checking every 5 seconds to send Gmail camera emails as discord messages
+    # Start checking every 5 minutes to send Gmail camera emails as discord messages
     # Added into the discord client event loop
-    client.loop.create_task(do_stuff_every_x_seconds(300, sendGmailAsDiscord, videoLabelId, client, gmailService, loadedConfig))
+    client.loop.create_task(do_stuff_every_x_seconds(300, sendGmailAsDiscord, videoLabelId, client, gmailService, loadedConfig, videoLabelSendingChannel))
+    # Start checking every 5 minutes to send Gmail chore notification emails as discord messages
+    # Added into the discord client event loop
+    client.loop.create_task(do_stuff_every_x_seconds(300, sendGmailSubjectAsDiscord, choreLabelId, client, gmailService, loadedConfig, choreLabelSendingChannel))
+
 # Starting the discord bot
 client.run(loadedConfig['discord']['clientToken'])
